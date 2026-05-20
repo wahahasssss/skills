c383ffe53c73038f674d09ba0ac00b104ea47d8c
killer <zhangzhengdi@outlook.com>
2026-05-20 02:58:45 +0000

    feat: number_check skill第一版本

---
+++ numbers_judg/main.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == "__main__":
+    # 打印所有参数
+    print("所有参数列表：", sys.argv)
+    content = sys.argv[1]
+    spit_str = sys.argv[2]
+    str_arr = content.split(",")
+    not_num = []
+    for s in str_arr:
+        try:
+            int(s)
+        except Exception as e:
+            not_num.append(s)
+    print(",".join(not_num))
